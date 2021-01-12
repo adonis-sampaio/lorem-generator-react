c5e56c66ab389a2f3ca157fdc92063ef981bf747
adonis-sampaio <adonissamp@gmail.com>
2021-01-12 15:55:33 -0200

    Lorem Generator without validation settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
   const [count, setCount] = useState();
   const [text, setText] = useState([]);
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello World');
+
+    let amount = parseInt(count);
+
+    setText(data.slice(0,amount));
+    console.log(text);
   }
 
   return (
@@ -17,10 +22,14 @@ function App() {
           Paragraphs: 
         </label>
         <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value) }/>
-        <button type="submit">Add</button>
+        <button type="submit" className="btn">Generate</button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
-  
     )
 }
 
